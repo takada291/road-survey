1af079e982270f81c23ff261136d0442c28c26b7
takada291 <tkp291@gmail.com>
2026-01-27 20:22:56 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'road-survey-v1.1.0';
+const CACHE_NAME = 'road-survey-v1.2.0';
 const urlsToCache = [
   './',
   './index.html',
@@ -57,6 +57,7 @@ self.addEventListener('message', (event) => {
   }
 
 });
+
 
 
 
